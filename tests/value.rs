a28bd1cbe4a6377175ff80eeb05b984288a8e008
John-John Tedro <udoprog@tedro.se>
2019-08-09 18:54:16 +0200

    Add test for normalization

--- tests/value.rs
+++ tests/value.rs
@@ -54,3 +54,24 @@ fn test_width() {
     assert_eq!(24, std::mem::size_of::<Integer>());
     assert_eq!(32, std::mem::size_of::<Key>());
 }
+
+#[test]
+fn test_normalize() {
+    let a = Key::Map(
+        vec![
+            (Key::String(String::from("baz")), Key::String(String::from("biz"))),
+            (Key::String(String::from("foo")), Key::String(String::from("bar"))),
+        ]
+    );
+
+    let b = Key::Map(
+        vec![
+            (Key::String(String::from("foo")), Key::String(String::from("bar"))),
+            (Key::String(String::from("baz")), Key::String(String::from("biz"))),
+        ]
+    );
+
+    assert_ne!(a, b);
+    assert_eq!(a, b.clone().normalize());
+    assert_eq!(a.clone().normalize(), b.clone().normalize());
+}
